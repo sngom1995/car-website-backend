c8eeb20a4db9cc756966e93161c2fbdce8a7f58e
sngom1995 <ngomsamba75@gmail.com>
2024-06-01 22:46:25 +0000

    add eureka server

--- vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -19,7 +18,6 @@ import org.springframework.web.reactive.function.client.WebClient;
  */
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableEurekaServer
 public class VehiclesApiApplication {
 
     public static void main(String[] args) {
